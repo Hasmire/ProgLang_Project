89aa1feac40e998c2399d2d5dee93ed11247e2f9
Lancelot <bulanlancelot@gmail.com>
2023-12-08 02:07:59 +0800

    Add Error Handling demo

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MyClasses\Animal;
 use App\Http\Controllers\MyClasses\Cat;
 use App\Http\Controllers\MyClasses\Circle;
 use App\Http\Controllers\MyClasses\Dog;
+use App\Http\Controllers\MyClasses\ErrorHandler;
 use App\Http\Controllers\MyClasses\MyClass;
 use App\Http\Controllers\MyClasses\Square;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -106,5 +107,53 @@ class Controller extends BaseController
 
         dump('Circle Area: ' . $circle->calculateArea());
         dump('Square Area: ' . $square->calculateArea());
+    }
+
+    public function errorHandling(){
+
+      echo '<div style="text-align: center;">';
+
+      echo '<div style="margin-right: 10px;">';
+      echo '<img src="'.asset('images/error-handling2.png').'" alt="Your Image" width="700">';
+      echo '</div>';
+
+      echo '<div style="margin-right: 10px;">';
+      echo '<img src="'.asset('images/error-handling3.png').'" alt="Your Image" width="700">';
+      echo '</div>';
+
+      echo '<div style="margin-right: 10px;">';
+      echo '<img src="'.asset('images/error-handling1.png').'" alt="Your Image" width="700">';
+      echo '</div>';
+
+      echo '</div>';
+
+      // Create an instance of ErrorHandler Class
+      $myClass = new ErrorHandler();
+
+      // Set the error handler to the user-defined error handler
+      $old_error_handler = set_error_handler([$myClass, 'myErrorHandler']);
+
+      // Trigger some errors using the MyClass methods
+      dump("vector a");
+      $a = [2, 3, "foo", 5.5, 43.3, 21.11];
+      dump($a);
+
+      // now generate second array
+      dump("vector b - a notice (b = log(PI) * a)");
+      // Warning about non-numeric values, and the result is printed.
+      $b = $myClass->scale_by_log($a, M_PI);
+      dump($b);
+
+      // pass a string instead of an array
+      dump("vector c - a warning");
+      // Warning about incorrect input, and var_dump shows that the function returns NULL.
+      $c = $myClass->scale_by_log("not array", 2.3);
+      dump($c);
+
+      // Fatal error about the logarithm of a non-positive number, and var_dump is not reached as the script terminates.
+      dump("vector d - fatal error");
+      $d = $myClass->scale_by_log($a, -2.5);
+      dump($d); // Never reached
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::view('server-side', 'demos.server-side-scripting');
 Route::view('easy-integration', 'demos.easy-integration');
 Route::get('procedural-object-oriented', [Controller::class, 'demoProceduralObject']);
 
+// Numbers 5-8 [Lancelot]
+Route::get('error-handling', [Controller::class, 'errorHandling']);
+
 // Numbers 9 to 12 [Jay]
 Route::get('binding', [Controller::class, 'demoBinding']);
 Route::get('data-type', [Controller::class, 'demoDataType']);
