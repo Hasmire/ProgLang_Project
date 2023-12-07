5cc026c9ad1bcc22fdd7282ba2544303fac28dda
jay <86312510+mojay2@users.noreply.github.com>
2023-12-07 13:21:36 +0800

    Add binding demo

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function demoBinding()
+    {
+        echo '<img src="'.asset('images/binding_snippet.png').'" alt="Your Image" width="900">';
+        // Dynamic binding example
+        $variable = 10;  // integer
+        dump("Original Value: $variable");
+
+        $variable = 'Hello, world!';  // now a string
+        dump("New Value: $variable");
+        $variable = 3.14;  // now a float
+        dump("Updated Value: $variable");
+
+        // Automatic type conversion
+        $number = '5';  // $number is a string
+        $result = $number + 10;  // PHP will automatically convert $number to an integer for the addition
+        dump("Result: $result");
+    }
 }
