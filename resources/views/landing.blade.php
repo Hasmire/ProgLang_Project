1c69e961f37179e22b5b50df8d2e23dd590315d7
Hasmire <lagman.jrgreggvictor@gmail.com>
2023-12-07 11:30:19 +0800

    add laravel

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Welcome to Our Site</title>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <h1 h1 class="font-bold underline text-3xl">
+    Hello world!
+  </h1>
+</body>
+
+</html>
